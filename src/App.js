6d9b29176117f291e60bf9351eef57e57554443f
covenantebube <covenantebube1@gmail.com>
2023-04-11 12:14:37 -0700

    dynamic categories

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
 
 const App = () => {
+     const categories =[
+        {
+          id: 1,
+          title: 'Hats',
+        },
+        {
+          id: 2,
+          title: 'Jackets',
+        },
+        {
+          id: 3,
+          title: 'Sneakers',
+        },
+        {
+          id: 4,
+          title: 'Womens',
+        },
+        {
+          id: 5,
+          title: 'Mens',
+        },
+       
+     ];
+
   return (
     <div className="categories-container">
-   <div className="category-container">
-     {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
- 
-      </div>
+      {categories.map(({title})=>(
       <div className="category-container">
-     {/* <img/> */}
-      <div className="category-body-container">
-        <h2>JACKETS</h2>
-        <p>Shop Now</p>
-      </div>
- 
-      </div>
-      <div className="category-container">
-     {/* <img/> */}
-      <div className="category-body-container">
-        <h2>SNEAKERS</h2>
-        <p>Shop Now</p>
-      </div>
- 
-      </div>
-      <div className="category-container">
-     {/* <img/> */}
-      <div className="category-body-container">
-        <h2>WOMENS</h2>
-        <p>Shop Now</p>
-      </div>
-      <div className="category-container">
-     {/* <img/> */}
-      <div className="category-body-container">
-        <h2>MENS</h2>
-        <p>Shop Now</p>
-      </div>
- 
-      </div>
- 
-      </div>
-    </div>
- 
+      <div className="background-image"/>
+        {/* <img/> */}
+         <div className="category-body-container">
+           <h2>{title}</h2>
+           <p>Shop Now</p>
+         </div>
+        </div>
+        ))}
+       </div>
+  
   );
-};
+      };
 
 export default App;
