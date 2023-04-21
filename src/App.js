80507dd68294c814c597464817e32a0b1c904b86
covenantebube <covenantebube1@gmail.com>
2023-04-21 16:31:50 -0700

    finish authentication form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentcation from "./routes/authentication/authentication.component";
 
 const  Shop =()=>{
     return <h1>I am the shop page</h1>
@@ -17,7 +17,7 @@ const App = () => {
      <Route path='/'element ={<Navigation/>} > 
         <Route index element ={<Home/>} />
           <Route path='shop'element ={<Shop/>} />
-          <Route path='sign-in'element ={<SignIn/>} />
+          <Route path='auth'element ={<Authentcation/>} />
       </Route>
     </Routes>
        );
