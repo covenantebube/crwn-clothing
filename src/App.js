5cb152114913a8ddb8afd478af9f79b6b56f3d5c
covenantebube <covenantebube1@gmail.com>
2023-04-22 06:56:16 -0700

    adding cart context and shopping icon, drop down

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import { Routes, Route} from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentcation from "./routes/authentication/authentication.component";
+import { Routes, Route } from 'react-router-dom';
 
-const  Shop =()=>{
-    return <h1>I am the shop page</h1>
-
-}
-
-
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
-    return( 
-    
+  return (
     <Routes>
-     <Route path='/'element ={<Navigation/>} > 
-        <Route index element ={<Home/>} />
-          <Route path='shop'element ={<Shop/>} />
-          <Route path='auth'element ={<Authentcation/>} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
-       );
-    
-      };
-    
+  );
+};
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client"; // Update the import statement
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+import './index.scss';
+
+const rootElement = document.getElementById("root");
+
+createRoot(rootElement).render( // Use createRoot instead of render
+<React.StrictMode>
+<BrowserRouter>
+  <UserProvider>
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
+  </UserProvider>
+</BrowserRouter>
+</React.StrictMode>,
+rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
